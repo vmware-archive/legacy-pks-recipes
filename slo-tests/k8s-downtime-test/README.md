bb35ebd7e79801c9b3d74efced96d601600291f4
Biju Kunjummen <biju.kunjummen@gmail.com>
2018-09-25 16:14:25 -0700

    Script changes to account for login page

--- slo-tests/k8s-downtime-test/README.md
+++ slo-tests/k8s-downtime-test/README.md
@@ -1,3 +1,9 @@
+## Running a test
+
+The following kicks of the load test:
+
 ```
-./gradlew gatlingRun -DTARGET_URL=http://10.195.43.139:9080
+./gradlew gatlingRun -DTARGET_URL=http://10.195.43.139:9080 -DSIM_USERS=3 -DDURATION=1
 ```
+
+Note that DURATION above is in **minutes**
--- slo-tests/k8s-downtime-test/src/test/scala/simulations/DaytraderSimulation.scala
+++ slo-tests/k8s-downtime-test/src/test/scala/simulations/DaytraderSimulation.scala
@@ -8,7 +8,8 @@ import scala.concurrent.duration._
 class DaytraderSimulation extends Simulation {
 
   val baseUrl = System.getProperty("TARGET_URL")
-  val sim_users = 3
+  val simUsers = System.getProperty("SIM_USERS", "3").toInt
+  val repeatDuration = System.getProperty("DURATION", "5").toInt
 
   val httpConf = http
     .baseUrl(baseUrl)
@@ -16,13 +17,13 @@ class DaytraderSimulation extends Simulation {
 
   val headers = Map("Accept" -> "text/html")
 
-  val sampleUserFlow = during(5 minutes) {
+  val sampleUserFlow = during(repeatDuration minutes) {
     exec(http("daytrader home page")
       .get("/daytrader/").check(status.in(200, 304))
     ).pause(1 seconds)
       .exec(
         http("loginpage")
-          .get("/daytrader/app")
+          .get("/daytrader/app").check(substring("Log in"))
           .headers(headers)
       ).pause(1 second)
       .exec(
@@ -35,16 +36,27 @@ class DaytraderSimulation extends Simulation {
       ).pause(1 seconds)
       .exec(
         http("Account Summary")
-          .get("/daytrader/app?action=account")
+          .get("/daytrader/app?action=account").check(substring("429 Oak St.") )
           .headers(headers)
       ).pause(1 second)
       .exec(
+        http("Home")
+          .get("/daytrader/app?action=home").check(substring("Welcome"))
+          .headers(headers)
+      ).pause(1 second)
+
+      .exec(
         http("Portfolio Page")
-          .get("/daytrader/app?action=portfolio")
+          .get("/daytrader/app?action=portfolio").check(substring("of Holdings"))
+          .headers(headers)
+      ).pause(1 second)
+      .exec(
+        http("Quotes And Trades")
+          .get("/daytrader/app?action=quotes&symbols=s:0,s:1,s:2,s:3,s:4").check(substring("Quotes"))
           .headers(headers)
       ).pause(1 second)
       .exec(http("Logout")
-        .get("/daytrader/app?action=logout")
+        .get("/daytrader/app?action=logout").check(substring("Log in"))
         .headers(headers)
       ).pause(1 second)
   }
@@ -53,5 +65,5 @@ class DaytraderSimulation extends Simulation {
   val scn = scenario("Day Trader App")
     .exec(sampleUserFlow)
 
-  setUp(scn.inject(atOnceUsers(sim_users)).protocols(httpConf))
+  setUp(scn.inject(atOnceUsers(simUsers)).protocols(httpConf))
 }
