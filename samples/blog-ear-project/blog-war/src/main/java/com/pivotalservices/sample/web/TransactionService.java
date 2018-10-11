e2ccf4322d93d17c1ed56f38465709be975083b4
William Voorhees <wvoorhees@pivotal.io>
2018-10-11 13:58:45 -0700

    Distributed Transaction
    Got the XA transactions to work, by adding the right attribute on the database connection definition

--- samples/blog-ear-project/blog-war/src/main/java/com/pivotalservices/sample/web/TransactionService.java
+++ samples/blog-ear-project/blog-war/src/main/java/com/pivotalservices/sample/web/TransactionService.java
@@ -1,10 +1,6 @@
 package com.pivotalservices.sample.web;
 
-import com.pivotalservices.sample.batcher.SampleDataManager;
-import com.pivotalservices.sample.dao.AuditEntryDAO;
-import com.pivotalservices.sample.dao.PostDAO;
-
-import javax.ejb.EJB;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -13,38 +9,34 @@ import javax.ws.rs.Produces;
 @Produces({"application/json"})
 public class TransactionService {
 
-    @EJB
-    private AuditEntryDAO auditEntryDAO;
 
-    @EJB
-    private PostDAO postDAO;
-
+    @Inject
+    TransactionServiceEJB transactionServiceEJB;
 
     @Path("/Success")
     @GET
     public String performSuccessfulTransaction() {
         try {
-            //Just using USERID of 1 for now.
-            postDAO.create("Cross Database Transaction Post", "This post demonstrates the creation of a transaction that oppeartes across datasources", 1);
-            auditEntryDAO.create("This is a succesfull audit entry save.");
+            transactionServiceEJB.performSucessfulTransaction();
 
-        }catch (Exception ex){
+        } catch (Exception ex) {
             return "FAILURE";
         }
 
 
         return "SUCCESS";
+
+
     }
 
     @Path("/Failure")
     @GET
     public String performFailingTransaction() {
-        try{
+        try {
             //Just using USERID of 1 for now.
-            postDAO.create("Cross Database Transaction Post", "This post demonstrates the creation of a transaction that oppeartes across datasources", 1);
-            auditEntryDAO.createFailure("This is a succesfull audit entry save.");
+            transactionServiceEJB.performUnSucessfulTransaction();
 
-        }catch (Exception ex){
+        } catch (Exception ex) {
 
             return "FAILURE";
         }
@@ -52,7 +44,6 @@ public class TransactionService {
         return "SUCCESS";
 
     }
-
 
 
 }
--- samples/blog-ear-project/build-liberty-docker-image.sh
+++ samples/blog-ear-project/build-liberty-docker-image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t wsvoorhees/blog-ear-project:0.0.4-SNAPSHOT .
+docker build -t wsvoorhees/blog-ear-project:0.0.6-SNAPSHOT .
